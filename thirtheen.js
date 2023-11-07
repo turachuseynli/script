ae0bcf9a70653cbaede39c9139c635d66b2418be
turachuseynli <turachuseynli304@gmail.com>
2023-11-07 14:46:25 +0400

    script

--- thirtheen.js
+++ thirtheen.js
@@ -2,34 +2,39 @@ function add() {
     let money = Number(document.getElementById('money').value)
     let button = document.getElementById('btn')
     let paragraph = document.getElementById('div')
-     let a;
-     let b;
-     let c;
+    let a = 0 ;
+    let b = 0;
+    let c= 0;
+    let d= 0;
+    let e= 0;
+    let f= 0;
     if (isNaN(money) || money < 0) {
         paragraph.innerHTML = "Please enter valid number"
     }
-        if (money > 100) {
-             a = Math.floor(money / 100)
+    if (money > 1) {
+        //157
+        a = Math.floor(money % 5) //2
 
+        if (money > 5) {
+            d = Math.floor(((money % 10) - (money % 5)) / 5) //1
             if (money > 10) {
-                 b = Math.floor(((money% 100)-(money%10))/10)
+                b = Math.floor((((money % 100) - Math.floor((money % 100) / 50)*50 )-(Math.floor(((money % 100) - Math.floor((money % 100) / 50)*50 ) / 20)*20)-(money% 10)) / 10)
+                if (money > 20) {
+                    e = Math.floor((Math.floor((money % 100) - (Math.floor((money % 100) / 50)*50 )) )/20)
+                    if (money > 50) {
+                        f = Math.floor(((money % 100) - (money % 50)) / 50)
+                        if (money > 100) {
+                            c = Math.floor(money / 100)
+                        }
 
-
-                if (money > 1) {
-                   c = Math.floor(money % 10)
+                    }
                 }
-                else{
-                    paragraph.innerHTML = "0"
-                }
-                
-            }else{
-                paragraph.innerHTML = "0"
+               
+            }
         }
-    }
-    else{
-        paragraph.innerHTML = "0"
+        }
+    
+   
+    
+    paragraph.innerHTML = c + "-" + "100manat" + " " + f + "-" + "50manat" + " " + e + "-" + "20manat" + " " + b + "-" + "10manat" + " " + d + "-" + "5manat" + " " + a + "-" + "1manat";
 }
-
-    paragraph.innerHTML = a+"-"+"100manat"+" "+b+"-"+"10manat"+" "+c+"-"+"1manat";
-}
-
